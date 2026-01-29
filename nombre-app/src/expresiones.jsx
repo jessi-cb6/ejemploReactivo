ce9308d3e2ab38246872ce026fca50aa3f2f74c9
jessi-cb6 <240491@utxicotepec.edu.mx>
2026-01-29 11:56:50 -0600

    seguir

--- nombre-app/src/expresiones.jsx
+++ nombre-app/src/expresiones.jsx
@@ -1,45 +1,41 @@
+import './expresiones.css'
+import tarjet1 from './assets/tarjet1.jpg'
+import tarjet2 from './assets/tarjet2.jpg'
+import tarjet3 from './assets/tarjet3.jpg'
+
 function Expresiones(){
-    const nombre = 'Jessica';
-    const apellidos='Cruz'
     return(
-        <div>
-            <h2>Expresiones</h2>
-            <h3>Tu nombre es:{nombre } y tus apellidos son:{apellidos}</h3>
-            <Lista/>
+        <div className='tarjetas'>
+            <Tarjeta1/>
+            <Tarjeta2/>
+            <Tarjeta3/>
         </div>
     )
-    
 }
-
-function Lista(){
-    const users =[
-        {id: 1, name: 'Luis', role:'web designer'},
-        {id: 2, name: 'Jessi', role: 'developer'},
-        {id: 3, name: 'Juan', role:'webscript'},
-    ]
+function Tarjeta1(){
     return(
-        <div>
-            <table>
-                <tbody>
-                   <tr>
-                        <th> Nombre </th>
-                        <th> Role </th>
-                   </tr>
-                   <tr>
-                        {
-                            users.map(function(user,index){
-                                return (
-                                    <tr key={index}>
-                                    <td>(user.name)</td>
-                                    <td>(user.role)</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                   </tr> 
-                </tbody>
-            </table>
+<div className='uno'>
+    <img src={tarjet1} alt="" />
+    <h1>“Buen provecho y cero remordimientos.”</h1>
+</div>
+    )
+}
+function Tarjeta2(){
+    return(
+        <div className='dos'>
+            <img src={tarjet2} alt=""/>
+            <h1>“El amor entra por el estómago… y el postre también.”</h1>
         </div>
+
+    )
+}
+function Tarjeta3(){
+    return(
+       <div className='tres'>  
+         <img src={tarjet3} alt=""/>
+         <h1>“Buen provecho y cero remordimientos.”</h1>
+       </div>
+
     )
 }
 export default Expresiones
