6e18ab63fdef6bfc57ab9c988c57f1e63aec6d7a
jessi-cb6 <240491@utxicotepec.edu.mx>
2026-01-30 09:53:40 -0600

    hola

--- nombre-app/src/App.jsx
+++ nombre-app/src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
 import Encabezado from "./encabezado";
 import Expresiones from "./expresiones";
+import Pie from './piePagina';
 
-function App(){
-  return(
-    <div>
-    <Encabezado/>
-    <Expresiones/>
-    <h1>SC DSM</h1>
-    <h2>Jessica Cruz Barona</h2> 
+function App() {
+  return (
+
+    <div className='texto'>
+      <Encabezado/>
+      <Expresiones/>
+      
+  
+      
+      
+      <h1 className='texto1'>
+        WEBTOON es la plataforma líder en cómics digitales, destacando por su formato vertical a color, ideal para dispositivos móviles, con una amplia variedad de géneros. 
+         Los usuarios elogian la calidad artística y las historias adictivas
+      </h1>
+<Pie/>
     </div>
+
   )
 }
 
---
+++ nombre-app/src/piePagina.jsx
@@ -0,0 +1,10 @@
+import './Pagina.css'
+function Pie(){
+    return(
+        <div className="piepagina">
+            <footer> Derechos reservados por jessica cruz barona </footer>
+        </div>
+    )
+}
+
+export default Pie
